0e3ba3dc3f37e12da88c1ec4279d4eb04630471f
Bart Volkaerts <bart.volkaerts@nxp.com>
2020-01-28 15:53:36 +0100

    Updated menu_announcer with different announcements

--- menu_announcer.py
+++ menu_announcer.py
@@ -1,20 +1,29 @@
 from bs4 import BeautifulSoup
-import urllib.request
-import datetime
 from gtts import gTTS
+import datetime
+import os
+import random
 import subprocess
+import urllib.request
 
 now = datetime.datetime.now()
 dayname = now.strftime("%A")
 
 page = urllib.request.urlopen('https://sharedcampus.be/menu.html')
 d = page.read()
-found_day = False
+
+announcements = ['De menu van vandaag',
+                 'Vandaag op het menu',
+                 'Jammie, vandaag is het']
+
 
 def parse_menu(menu):
     print(menu)
     menu = menu.split('\n')
-    return "De menu van vandaag. Soep: {}, Schotel: {}, Suggestie: {}.".format(menu[1], menu[3], menu[5])
+    return "{}. {}. {}. Suggestie: {}.".format(random.choice(announcements),
+                                               menu[1],
+                                               menu[3],
+                                               menu[5])
 
 
 def speak_menu(menu):
@@ -27,9 +36,10 @@ def speak_menu(menu):
 
 
 soup = BeautifulSoup(d, "html.parser")
+found_day = False
 for tr in soup.findAll("table"):
     for td in tr.find_all("td"):
-        if found_day == False:
+        if found_day is False:
             if dayname in td.text:
                 found_day = True
         else:
